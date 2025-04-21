bd8e840169641f62b58accecb7287cdb3f8c66c1
HuzaifaSidiqui <huzaifa.siddique5000@gmail.com>
2025-04-22 00:55:19 +0500

    created auth routes register | login

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+//connect db
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(()=>{
+    console.log("Connected to MongoDB");
+}).catch((err)=>{
+    console.log(err);
+})
+
+app.use(express.json());
+
+app.use("/auth", require("./routes/user"));
+
+const port = process.env.PORT || 4001;
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
+})
+
+//test API
+app.get("/", (req,res)=>{
+    res.send("API is working");
+})
---
+++ routes/user.js
@@ -0,0 +1,53 @@
+const router = require('express').Router();
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+router.post('/register', async(req,res)=>{
+    const {name,email,password} = req.body;
+    try {
+        if(!name || !email || !password){
+            return res.status(400).json({message: "All fields are required"})
+        }
+        const existingUser = await User.findOne({email});
+        if(existingUser){
+            return res.status(400).json({message: "User already exists"})
+        }
+        const hashed_password = await bcrypt.hash(password, 10);
+        const newUser = new User({
+            name,
+            email,
+            password: hashed_password
+        })
+        await newUser.save();
+        res.status(201).json({message: "New user created"});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: "Internal server error"});
+    }
+})
+
+//login route
+router.post('/login', async (req,res)=>{
+    const {email, password} = req.bdy;
+    try {
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({message: "User not found"});
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(400).json({message: "Invalid credentials"});
+        }
+        const token = jwt.sign({
+            _id: user._id}, 
+            process.env.TOKEN_SECRET, 
+            {expiresIn: '1hr'});
+        return res.json(token);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: "Internal server error"});
+    }
+})
+
+module.exports = router;
